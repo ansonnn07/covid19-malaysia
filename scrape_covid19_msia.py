33f5ed426eff906994e06ebb819b97882ec347b5
Anson <roxastan@hotmail.com>
2021-04-21 18:55:10 +0800

    begin testing new format scraping

--- scrape_covid19_msia.py
+++ scrape_covid19_msia.py
@@ -23,7 +23,7 @@ month_translation = {"January": "januari",
                                   "November": "november",
                                   "December": "disember"}
 
-cases_to_extract_old = ["pulih", "kumulatif kes yang telah pulih", "kes baharu",
+cases_to_extract_old = ['(pulih|sembuh)', "kumulatif kes (yang telah pulih|sembuh)", "kes baharu",
                         "jumlah kes positif", "Unit Rawatan Rapi",
                         "pernafasan", "kes kematian", "kumulatif kes kematian"]
 
@@ -37,7 +37,8 @@ column_names = ["Date", "Recovered", "Cumulative Recovered", "Imported Case",
                         "Cumulative Case", "ICU", "Ventilator",
                         "Death", "Cumulative Death", "URL"]
 
-case_name_mapping = {"pulih": "Recovered", "kumulatif kes yang telah pulih": "Cumulative Recovered",
+case_name_mapping = {'(pulih|sembuh)': "Recovered",
+                     "kumulatif kes (yang telah pulih|sembuh)": "Cumulative Recovered",
                      "kes baharu": "New Case", "jumlah kes positif": "Cumulative Case",
                      "Unit Rawatan Rapi": "ICU", "pernafasan": "Ventilator",
                      "kes kematian": "Death", "kumulatif kes kematian": "Cumulative Death",
@@ -52,7 +53,8 @@ case_name_mapping = {"pulih": "Recovered", "kumulatif kes yang telah pulih": "Cu
 default_url = "https://kpkesihatan.com/{format1}/kenyataan-akhbar-kpk-{format2}-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
 
 special_dates = ['16-Apr-2020', '13-May-2020', '28-May-2020',
-                 '18-Jun-2020', '16-Jul-2020', '26-Jul-2020']
+                 '18-Jun-2020', '16-Jul-2020', '26-Jul-2020',
+                 '10-Dec-2020']
 special_dt = [datetime.strptime(i, '%d-%b-%Y')
               for i in special_dates]
 special_urls = ["https://kpkesihatan.com/2020/04/16/kenyataan-akhbar-16-april-2020-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/",
@@ -60,7 +62,8 @@ special_urls = ["https://kpkesihatan.com/2020/04/16/kenyataan-akhbar-16-april-20
                 "https://kpkesihatan.com/2020/05/28/situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/",
                 "https://kpkesihatan.com/2020/06/18/kenyataan-akhbar-kpk-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/",
                 "https://kpkesihatan.com/2020/07/16/kenyataan-akhbar-kkm-16-julai-2020-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/",
-                "https://kpkesihatan.com/2020/07/26/kenyataan-akhbar-kementerian-kesihatan-malaysia-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"]
+                "https://kpkesihatan.com/2020/07/26/kenyataan-akhbar-kementerian-kesihatan-malaysia-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/",
+                "https://kpkesihatan.com/2020/12/10/kenyataan-akhbar-kpk-9-disember-2020-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia-2/"]
 
 
 class Scraper:
@@ -123,9 +126,14 @@ class Scraper:
         return matched_number
 
     def find_text_and_numbers(self, txt, all_text):
+        if self.current_txt == 'Unit Rawatan Rapi':
+            # to avoid taking the numbers for the 'pernafasan'
+            regex_text = rf"([^.,\n]*(?:Unit Rawatan Rapi))"
+        else:
+            regex_text = rf"([^.,\n]*{txt}[^.,]*[,.]+)"
+
         if self.current_txt == 'kes kematian':
-            sentence_list = list(re.finditer(
-                rf"([^.,\n]*?{txt}[^.,]*[,.]+)", all_text))
+            sentence_list = list(re.finditer(regex_text, all_text))
 
             # to avoid sentences with strings like "ke-1234"
             first_sentence = sentence_list[0].group()
@@ -138,7 +146,7 @@ class Scraper:
         else:
             # straight away take the first found sentence using `re.search`
             # sentenceObj = re.search(rf"([^.\n]*?{txt}[^.]*\.)", all_text)
-            sentenceObj = re.search(rf"([^.,\n]*?{txt}[^.,]*[,.]+)", all_text)
+            sentenceObj = re.search(regex_text, all_text)
 
         if not sentenceObj:
             # raise Exception(f"[ERROR] {txt} not found!")
@@ -164,8 +172,8 @@ class Scraper:
         new_number = comma_sep_digits.replace(',', '')
         return new_number
 
-    def scrape_data(self, current_url, verbose=0):
-        r = requests.get(current_url)
+    def scrape_data(self, verbose=0):
+        r = requests.get(self.current_url)
         if r.status_code == 404:
             raise Exception("Error 404 accessing page!!")
         soup = BeautifulSoup(r.content, "lxml")
@@ -173,16 +181,10 @@ class Scraper:
         # Remove all COVID-19 words to avoid getting number 19 accidentally
         all_text = all_text.replace('COVID-19', '')\
             .replace('covid19', '')
-
         all_text = re.sub(r"\d+,\d+", self.replace_comma_sep_digits, all_text)
 
         data_dict = {}
         txt_to_skip = []
-
-        # if 'tiada kes kematian berkaitan' in all_text or current_url == problem_url_1:
-        #     data_dict['Death'] = 0
-        #     data_dict['Cumulative Death'] = np.nan
-        #     txt_to_skip = ('kes kematian', 'kumulatif kes kematian')
 
         cases_to_extract = cases_to_extract_old.copy()
 
@@ -197,39 +199,40 @@ class Scraper:
                 if verbose:
                     print(f"[INFO] Finding {txt} ...")
 
-                if txt == 'pulih':
-                    try:
-                        txt_found, numbers_found = self.find_text_and_numbers(
-                            txt, all_text)
-                    except Exception as e:
-                        # raise Exception(f"{e.__class__} has occurred!")
-                        print('[ERROR] "pulih" not found !')
-                        print(current_url)
-                        print('Proceeding to scraping using new text format ...')
+                if txt == '(pulih|sembuh)':
+                    if 'Kes sembuh :' in all_text:
+                        print(self.current_url)
+                        print("\n[WARNING] Proceeding to scraping "
+                              "using new text format ...\n")
                         # not the same text anymore, proceed to new format
                         return None
+                    else:
+                        txt_found, numbers_found = self.find_text_and_numbers(
+                            txt, all_text)
+
                 elif txt in ('kes baharu', 'jumlah kes positif'):
                     txt_to_search = "JUMLAH KESELURUHAN"
-                    # sentenceObj = list(re.finditer(
-                    #     rf"([^.\n]*?{txt_to_search}[^.]*\.)", all_text))[-1]
-                    # sentence = sentenceObj.group()
-                    # txt_found = re.search(txt_to_search, sentence)
-                    # numbers_found = list(re.finditer(r'[^(]\d+[^)]', sentence))
-                    sentence = [tags.text for tags in soup.find_all(
+                    # the correct index for the number
+                    number_idx = 1 if txt == "kes baharu" else 2
+                    # find all table rows
+                    sentence_tag = [tags for tags in soup.find_all(
                         "tr") if txt_to_search in tags.text][0]
-                    sentence = sentence.replace(',', '')
-                    # to remove the digits surrounded by parenthesis
-                    #  e.g. (4)
-                    sentence = re.sub(r'\(\d+\)', ' ', sentence)
-                    txt_found = re.search(txt_to_search, sentence)
-                    # numbers_found = list(re.finditer(
-                    #     r"[^A-Z()]*\d+[^)]", sentence))
-                    numbers_found = list(re.finditer(r"\d+", sentence))
+                    # find the table data with the correct index
+                    matched_number_str = sentence_tag.find_all("td")[
+                        number_idx].text
+                    # replace any comma separating the digits
+                    matched_number_str = matched_number_str.replace(',', '')\
+                        .replace(' ', '')
+                    # replace any number surrounded by parenthesis
+                    matched_number_str = re.sub(
+                        r'\(.+\)', ' ', matched_number_str)
                 else:
                     txt_found, numbers_found = self.find_text_and_numbers(
                         txt, all_text)
 
-                if not txt_found and not numbers_found:
+                if txt in ('kes baharu', 'jumlah kes positif'):
+                    matched_number = int(matched_number_str)
+                elif not txt_found and not numbers_found:
                     print(
                         f"[INFO] 'tiada' and no digit found in the sentence with '{txt}'")
                     print("Setting matched_number to 0.")
@@ -262,8 +265,8 @@ class Scraper:
 
         return data_dict
 
-    def scrape_data_2(self, current_url, verbose=0):
-        r = requests.get(current_url)
+    def scrape_data_2(self, verbose=0):
+        r = requests.get(self.current_url)
         if r.status_code == 404:
             raise Exception("Error 404 accessing page!!")
         soup = BeautifulSoup(r.content, "lxml")
@@ -342,15 +345,15 @@ class Scraper:
         for day_number in range(self.total_days):
             print(f"[INFO] Scraping data for {self.current_date.date()} "
                   f"({day_number}/{self.total_days}) ...")
-            current_url = default_url.format(**self.current_date_dict)
-            # print(current_url)
+            self.current_url = default_url.format(**self.current_date_dict)
+            # print(self.current_url)
             if self.current_date in special_dt:
-                current_url = special_urls[special_dt.index(
+                self.current_url = special_urls[special_dt.index(
                     self.current_date)]
             try:
                 if not self.new_format_flag:
                     # still in old text format, scrape using old method
-                    data_dict = self.scrape_data(current_url, verbose=verbose)
+                    data_dict = self.scrape_data(verbose=0)
 
                 if not data_dict:
                     print("[ATTENTION] USING NEW text format "
@@ -358,14 +361,13 @@ class Scraper:
                     sys.exit("Stopping to modify the new format for now")
                     # using new text format method
                     self.new_format_flag = True
-                    data_dict = self.scrape_data_2(
-                        current_url, verbose=verbose)
+                    data_dict = self.scrape_data_2(verbose=verbose)
 
                 # print(data_dict)
 
                 # df.loc[current_date] = data_dict
                 data_dict["Date"] = self.current_date
-                data_dict["URL"] = current_url
+                data_dict["URL"] = self.current_url
                 self.df = self.df.append(data_dict, ignore_index=True)
 
                 self.current_date += timedelta(days=1)
@@ -376,39 +378,57 @@ class Scraper:
                 filename = f"{self.start_date.date()}_{self.current_date.date()}.csv"
                 self.df.to_csv(os.path.join(
                     "csv_files", filename), index=False)
-                print("[ERROR] Problem with", current_url)
+                print("[ERROR] Problem with", self.current_url)
                 raise Exception(f"Error on {self.current_date.date()}")
             # print()
             # break
         filename = f"{self.start_date.date()}_{self.end_date.date()}.csv"
         self.df.to_csv(os.path.join("csv_files", filename), index=False)
+        print("\n[INFO] CONGRATS! You have scraped until the end date!")
 
-    def test_scrape_first_day(self):
-        current_url = default_url.format(**self.start_date_dict)
-        r = requests.get(current_url)
+    def test_scrape_first_day(self, verbose=0):
+        self.current_url = default_url.format(**self.start_date_dict)
+        r = requests.get(self.current_url)
         if r.status_code == 404:
             raise Exception("Error 404 accessing page!!")
 
-        data_dict = self.scrape_data(current_url)
-        print(current_url)
+        data_dict = self.scrape_data(verbose=verbose)
+        print(self.current_url)
         display(data_dict)
         return data_dict
 
+    def continue_scraping(self, verbose=0):
+        self.total_days = (self.end_date - self.current_date).days + 1
+        self.scrape(verbose=verbose)
 
-# start_date = Scraper.create_datetime(day=17, month=10, year=2020)
-start_date = Scraper.create_datetime(day=27, month=3, year=2020)
-end_date = Scraper.create_datetime(day=12, month=12, year=2020)
+
+# FIRST DATE TO SCRAPE
+# start_date = Scraper.create_datetime(day=27, month=3, year=2020)
+# Final date before new format
+start_date = Scraper.create_datetime(day=19, month=1, year=2021)
+
+# NEW FORMAT ON 2021-01-20
+# start_date = Scraper.create_datetime(day=20, month=1, year=2021)
+start_date = Scraper.create_datetime(day=20, month=1, year=2021)
+end_date = Scraper.create_datetime(day=12, month=2, year=2021)
+
+# FINAL DATE TO SCRAPE SO FAR
+end_date = Scraper.create_datetime(day=15, month=4, year=2021)
 
 scraper = Scraper(start_date, end_date)
 
 verbose = 0
 # scraper.scrape(verbose=verbose)
 
-data_dict = scraper.test_scrape_first_day()
+data_dict = scraper.test_scrape_first_day(verbose=verbose)
+
+# scraper.continue_scraping(verbose=verbose)
 
 test_scrape = 0
 if test_scrape:
-    current_url = "https://kpkesihatan.com/2020/03/27/kenyataan-akhbar-kpk-27-mac-2020-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
+    # current_url = "https://kpkesihatan.com/2021/04/17/kenyataan-akhbar-kpk-17-april-2021-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
+    current_url = "https://kpkesihatan.com/2021/04/17/kenyataan-akhbar-kpk-17-april-2021-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
+    # current_url = "https://kpkesihatan.com/2020/03/27/kenyataan-akhbar-kpk-27-mac-2020-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
     r = requests.get(current_url)
     if r.status_code == 404:
         raise Exception("Error 404 accessing page!!")
@@ -425,23 +445,54 @@ if test_scrape:
 
     # all_text = re.sub(r'\d+,')
 
+    # if 'Kes sembuh :' in all_text:
+    #     print(current_url)
+    #     print('\n[WARNING] Proceeding to scraping using new text format ...\n')
+
     test_text = 'Sehingga kini, terdapat 12,913\xa0kes\xa0positif  yang sedang dirawat di Unit Rawatan Rapi (ICU),\xa0di mana 30 kes memerlukan bantuan pernafasan.'
 
     test_text = re.sub(
         r"\d+,\d+", repl=replace_comma_sep_digits, string=test_text)
 
-    txt = 'Unit Rawatan Rapi'
-    # sentence = re.search(rf"([^.\n]*{txt}[^.]*[.]+)", test_text).group()
-    sentence = re.search(rf"([^.,\n]*?{txt}[^.,]*[.,]+)", test_text).group()
-    txt_found = re.search(txt, sentence)
-    numbers_found = list(re.finditer(r"\d+,*\d+", sentence))
+    # txt = 'Unit Rawatan Rapi'
+    # # sentence = re.search(rf"([^.\n]*{txt}[^.]*[.]+)", test_text).group()
+    # sentence = re.search(rf"([^.,\n]*{txt}[^.,]*[.,]+)", test_text).group()
+    # txt_found = re.search(txt, sentence)
+    # numbers_found = list(re.finditer(r"\d+,*\d+", sentence))
 
     txt_to_search = "JUMLAH KESELURUHAN"
-    sentence = str([tags for tags in soup.find_all(
-        "tr") if "JUMLAH KESELURUHAN" in tags.text][0])
-    sentence = sentence.replace(',', '')
-    sentence = re.sub(r'\(\d+\)', ' ', sentence)
-    txt_found = re.search(txt_to_search, sentence)
-    # numbers_found = list(re.finditer(r"[^A-Z()]*\d+[^)]", sentence))
-    # numbers_found = list(re.finditer(r"[^A-Z()\w]*\d+[^)\w]*", sentence))
-    numbers_found = list(re.finditer(r"\d+,*\d+", sentence))
+    # txt = "kes baharu"
+    test_txt = "jumlah kes positif"
+    # the correct index for the number
+    number_idx = 1 if test_txt == "kes baharu" else 2
+    sentence_tag = [tags for tags in soup.find_all(
+        "tr") if txt_to_search in tags.text][0]
+    matched_number_str = sentence_tag.find_all("td")[number_idx].text
+    matched_number_str = matched_number_str.replace(',', '').replace(' ', '')
+    matched_number_str = re.sub(r'\(.+\)', ' ', matched_number_str)
+    matched_number = int(matched_number_str)
+
+test_scrape_new = 0
+if test_scrape_new:
+    # current_url = "https://kpkesihatan.com/2021/01/20/kenyataan-akhbar-kpk-20-januari-2021-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
+    current_url = "https://kpkesihatan.com/2021/01/20/kenyataan-akhbar-kpk-20-januari-2021-situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia/"
+    r = requests.get(current_url)
+    if r.status_code == 404:
+        raise Exception("Error 404 accessing page!!")
+    soup = BeautifulSoup(r.content, "lxml")
+    all_text = soup.get_text()
+
+    def replace_comma_sep_digits(matchObj):
+        comma_sep_digits = matchObj.group()
+        new_number = comma_sep_digits.replace(',', '').replace(' ', '')
+        return new_number
+
+    all_case_ul = soup.find_all("ul")[1]
+    li_text_list = [li_tag.text for li_tag in all_case_ul]
+    li_text_list = [re.sub(r"\d+,\d+", replace_comma_sep_digits, txt)
+                    for txt in li_text_list]
+
+    test_text = 'Sehingga kini, terdapat 12,913\xa0kes\xa0positif  yang sedang dirawat di Unit Rawatan Rapi (ICU),\xa0di mana 30 kes memerlukan bantuan pernafasan.'
+
+    test_text = re.sub(
+        r"\d+,\d+", repl=replace_comma_sep_digits, string=test_text)
